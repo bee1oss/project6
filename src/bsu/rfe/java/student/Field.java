661c9bc791c65d9a330b31c4bdabb1d771f56ab4
bee1oss <begahaciyew@gmail.com>
2022-03-25 13:08:33 +0300

    starting
    writing some code

--- src/bsu/rfe/java/student/Field.java
+++ src/bsu/rfe/java/student/Field.java
@@ -1,12 +1,14 @@
 package bsu.rfe.java.student;
 
 //calismak lazim...
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import javax.swing.*;
+import java.util.ArrayList;
 
 
+@SuppressWarnings("serial")
+public class Field extends JPanel {
 
-public class Field {
-
+    private boolean paused;
+    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+    private Time
 }
